ccc8de9e51fbe72603fabca54d0a3c33bacc7cad
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2018-09-14 16:01:26 +0200

    Add Redux that isn't hooked into any ui yet

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'));
