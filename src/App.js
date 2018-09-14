4e5005153a1c9637e41dfbb5cad01f60beab1218
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2018-09-14 14:36:28 +0200

    More configs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Config from './config.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" proc={process}>
-        url: <p>{process.env.PUBLIC_URL}</p>!
+        <div className="App" proc={process} conf={Config}>
+        url: {process.env.PUBLIC_URL}
+        custom config: 
       </div>
     );
   }
