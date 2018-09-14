e6c9cb7641860478bd0fbb2a0f29cf2e6a6f3c70
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2018-09-14 14:54:44 +0200

    Add material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Config from './config.js';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
-        <div className="App" proc={process} conf={Config}>
+      <div className="App" proc={process} conf={Config}>
         url: {process.env.PUBLIC_URL}
-        custom config: 
+        custom config: {Config.s3.BUCKET}
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
