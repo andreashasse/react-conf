947b064b711e3e82c19ae19bd90856e889193a3f
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2018-10-04 09:47:17 +0200

    Minor fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+const store = createStore(rootReducer);
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
 
 ReactDOM.render(
